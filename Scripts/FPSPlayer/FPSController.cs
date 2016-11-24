3f66beb725bfa7a524bab5a6d2ecbee0f837d1ec
thusitha@crowderia.com <thusitha@crowderia.com>
2016-11-25 01:48:20 +0530

    player movement and player shoot

---
+++ Scripts/FPSPlayer/FPSController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(FPSMotor))]
+public class FPSController : MonoBehaviour {
+
+	// Component caching
+	private FPSMotor motor;
+
+	void Start ()
+	{
+		motor = GetComponent<FPSMotor>();
+	}
+
+	void Update ()
+	{
+		//Calculate movement velocity as a 3D vector
+		float _xMov = Input.GetAxis("Horizontal");
+		float _zMov = Input.GetAxis("Vertical");
+
+		Vector3 _movHorizontal = transform.right * _xMov;
+		Vector3 _movVertical = transform.forward * _zMov;
+
+		// Final movement vector
+		Vector3 _velocity = (_movHorizontal + _movVertical);
+
+		//Apply movement
+		motor.Move(_velocity);
+
+	}
+}
